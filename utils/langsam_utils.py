1aead5ad86f91d3cbf518f4e501c7499b575a66a
Isabelle Tingzon <issatingzon@gmail.com>
2023-07-04 11:48:24 +0800

    Fix langsam_utils bugs

--- utils/langsam_utils.py
+++ utils/langsam_utils.py
@@ -35,17 +35,21 @@ import itertools
 SAM_MODELS = {
     "vit_h": "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth",
     "vit_l": "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_l_0b3195.pth",
-    "vit_b": "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth"
+    "vit_b": "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth",
 }
 
 # Default cache path for model checkpoints
-CACHE_PATH = os.environ.get("TORCH_HOME", os.path.expanduser("~/.cache/torch/hub/checkpoints"))
+CACHE_PATH = os.environ.get(
+    "TORCH_HOME", os.path.expanduser("~/.cache/torch/hub/checkpoints")
+)
+
 
 # Class definition for LangSAM
 class LangSAM:
     """
     A Language-based Segment-Anything Model (LangSAM) class which combines GroundingDINO and SAM.
     """
+
     def __init__(self, sam_type: str = "vit_h"):
         """
         Initialize the LangSAM instance.
@@ -54,7 +58,9 @@ class LangSAM:
         sam_type (str): Type of SAM model to use. Default is "vit_h".
         """
         if sam_type not in SAM_MODELS:
-            raise ValueError(f"Invalid SAM model type. Available options are {list(SAM_MODELS.keys())}.")
+            raise ValueError(
+                f"Invalid SAM model type. Available options are {list(SAM_MODELS.keys())}."
+            )
 
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.build_groundingdino()
@@ -81,7 +87,9 @@ class LangSAM:
         ckpt_repo_id = "ShilongLiu/GroundingDINO"
         ckpt_filename = "groundingdino_swinb_cogcoor.pth"
         ckpt_config_filename = "GroundingDINO_SwinB.cfg.py"
-        self.groundingdino = load_model_hf(ckpt_repo_id, ckpt_filename, ckpt_config_filename, self.device)
+        self.groundingdino = load_model_hf(
+            ckpt_repo_id, ckpt_filename, ckpt_config_filename, self.device
+        )
 
     def predict_dino(self, image_pil, text_prompt, box_threshold, text_threshold):
         """
@@ -97,14 +105,18 @@ class LangSAM:
         Tuple containing boxes, logits, and phrases.
         """
         image_trans = transform_image(image_pil)
-        boxes, logits, phrases = predict(model=self.groundingdino,
-                                         image=image_trans,
-                                         caption=text_prompt,
-                                         box_threshold=box_threshold,
-                                         text_threshold=text_threshold,
-                                         device=self.device)
+        boxes, logits, phrases = predict(
+            model=self.groundingdino,
+            image=image_trans,
+            caption=text_prompt,
+            box_threshold=box_threshold,
+            text_threshold=text_threshold,
+            device=self.device,
+        )
         W, H = image_pil.size
-        boxes = box_ops.box_cxcywh_to_xyxy(boxes) * torch.Tensor([W, H, W, H]).to(boxes.device)  
+        boxes = box_ops.box_cxcywh_to_xyxy(boxes) * torch.Tensor([W, H, W, H]).to(
+            boxes.device
+        )
         # Ensure tensor is on the same device
         return boxes, logits, phrases
 
@@ -121,7 +133,9 @@ class LangSAM:
         """
         image_array = np.array(image_pil)
         self.sam.set_image(image_array)
-        transformed_boxes = self.sam.transform.apply_boxes_torch(boxes, image_array.shape[:2])
+        transformed_boxes = self.sam.transform.apply_boxes_torch(
+            boxes, image_array.shape[:2]
+        )
         masks, _, _ = self.sam.predict_torch(
             point_coords=None,
             point_labels=None,
@@ -130,7 +144,13 @@ class LangSAM:
         )
         return masks.cpu()
 
-    def predict(self, image_pil: Image, text_prompt: str, box_threshold: float, text_threshold: float):
+    def predict(
+        self,
+        image_pil: Image,
+        text_prompt: str,
+        box_threshold: float,
+        text_threshold: float,
+    ):
         """
         Run both GroundingDINO and SAM model prediction.
 
@@ -143,16 +163,20 @@ class LangSAM:
         Returns:
         Tuple containing masks, boxes, phrases, and logits.
         """
-        boxes, logits, phrases = self.predict_dino(image_pil, text_prompt, box_threshold, text_threshold)
+        boxes, logits, phrases = self.predict_dino(
+            image_pil, text_prompt, box_threshold, text_threshold
+        )
         masks = torch.tensor([])
         if len(boxes) > 0:
             masks = self.predict_sam(image_pil, boxes)
             masks = masks.squeeze(1)
         return masks, boxes, phrases, logits
-    
-    
+
+
 # Define helper functions
-def load_model_hf(repo_id: str, filename: str, ckpt_config_filename: str, device: str = 'cpu') -> torch.nn.Module:
+def load_model_hf(
+    repo_id: str, filename: str, ckpt_config_filename: str, device: str = "cpu"
+) -> torch.nn.Module:
     """
     Loads a model from HuggingFace Model Hub.
 
@@ -168,7 +192,9 @@ def load_model_hf(repo_id: str, filename: str, ckpt_config_filename: str, device
     # Ensure the repo ID and filenames are valid
     assert isinstance(repo_id, str) and repo_id, "Invalid repository ID"
     assert isinstance(filename, str) and filename, "Invalid model filename"
-    assert isinstance(ckpt_config_filename, str) and ckpt_config_filename, "Invalid config filename"
+    assert (
+        isinstance(ckpt_config_filename, str) and ckpt_config_filename
+    ), "Invalid config filename"
 
     # Download the config file and build the model from it
     cache_config_file = hf_hub_download(repo_id=repo_id, filename=ckpt_config_filename)
@@ -179,11 +205,11 @@ def load_model_hf(repo_id: str, filename: str, ckpt_config_filename: str, device
     # Download the model checkpoint and load it into the model
     cache_file = hf_hub_download(repo_id=repo_id, filename=filename)
     checkpoint = torch.load(cache_file, map_location=device)
-    model.load_state_dict(clean_state_dict(checkpoint['model']), strict=False)
+    model.load_state_dict(clean_state_dict(checkpoint["model"]), strict=False)
     model.eval()
 
     return model
-        
+
 
 def transform_image(image: Image) -> torch.Tensor:
     """
@@ -195,87 +221,87 @@ def transform_image(image: Image) -> torch.Tensor:
     Returns:
     torch.Tensor: The transformed image as a tensor.
     """
-    transform = T.Compose([
-        T.RandomResize([800], max_size=1333),
-        T.ToTensor(),
-        T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-    ])
+    transform = T.Compose(
+        [
+            T.RandomResize([800], max_size=1333),
+            T.ToTensor(),
+            T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+        ]
+    )
     image_transformed, _ = transform(image, None)
     return image_transformed
 
 
 def model_predict(
-    image_file, 
-    model, 
-    text_prompt, 
-    box_threshold=0.3, 
-    text_threshold=0.3, 
+    image_file,
+    model,
+    text_prompt,
+    box_threshold=0.3,
+    text_threshold=0.3,
     out_file=None,
-    visualize=True
+    visualize=True,
 ):
-    #print(f"Reading {image_file}...")
     with rio.open(image_file) as src:
-        image_np = src.read().transpose((1, 2, 0))  # Convert rio image to numpy array
-        transform = src.transform  # Save georeferencing information
-        crs = src.crs  # Save the Coordinate Reference System
+        image_np = src.read().transpose((1, 2, 0))
+        transform = src.transform  
+        crs = src.crs 
     image_pil = Image.fromarray(image_np[:, :, :3])
-    
-    #print("Generating predictions...")
-    masks, boxes, phrases, logit = model.predict(
-        image_pil, text_prompt, box_threshold=0.3, text_threshold=0.3
-    )
-    
-    # Generate mask overlay
-    mask_overlay = np.zeros_like(image_np[..., 0], dtype=np.uint8)  
-    for i, (box, mask) in enumerate(zip(boxes, masks)):
-        # Convert tensor to numpy array if necessary and ensure it contains integers
-        if isinstance(mask, torch.Tensor):
-            mask = mask.cpu().numpy().astype(np.uint8)  # If mask is on GPU, use .cpu() before .numpy()
-        mask_overlay += ((mask > 0) * (i + 1)).astype(np.uint8)  # Assign a unique value for each mask
 
-    # Normalize mask_overlay to be in [0, 255]
-    mask_overlay = (mask_overlay > 0) * 255  # Binary mask in [0, 255]
-    
-    if out_file:
-        #print(f"Saving predictions to {out_file}...")
-        save_predictions(out_file, mask_overlay, transform, crs)
-    
-    if visualize:
-        #print("Preparing visualization...")
-        visualize_predictions(image_pil, boxes, mask_overlay)
-        
+    masks, boxes, phrases, logit = model.predict(
+        image_pil, 
+        text_prompt, 
+        box_threshold=box_threshold, 
+        text_threshold=text_threshold
+    )
     torch.cuda.empty_cache()
 
-    
+    # Generate mask overlay
+    mask_overlay = np.zeros_like(image_np[..., 0], dtype=np.uint8)
+    for i, (box, mask) in enumerate(zip(boxes, masks)):
+        if isinstance(mask, torch.Tensor):
+            mask = (
+                mask.cpu().numpy().astype(np.uint8)
+            )  
+        mask_overlay += ((mask > 0) * (i + 1)).astype(
+            np.uint8
+        )  
+    mask_overlay = (mask_overlay > 0) * 255  
+
+    if out_file:
+        save_predictions(out_file, mask_overlay, transform, crs)
+
+    if visualize:
+        visualize_predictions(image_pil, boxes, mask_overlay)
+
+
 def visualize_predictions(image_pil, boxes, mask_overlay):
     plt.figure(figsize=(10, 10))
     plt.imshow(image_pil)
 
     for box in boxes:
-        # Draw bounding box
-        box = box.cpu().numpy()  # Convert the tensor to a numpy array
+        box = box.cpu().numpy()  
         rect = patches.Rectangle(
-            (box[0], box[1]), 
-            box[2] - box[0], 
-            box[3] - box[1], 
-            linewidth=1, 
-            edgecolor='r', 
-            facecolor='none'
+            (box[0], box[1]),
+            box[2] - box[0],
+            box[3] - box[1],
+            linewidth=1,
+            edgecolor="r",
+            facecolor="none",
         )
         plt.gca().add_patch(rect)
 
-    plt.imshow(mask_overlay, cmap='viridis', alpha=0.4)  # Overlay the mask with some transparency
+    plt.imshow(mask_overlay, cmap="viridis", alpha=0.4)  
     plt.title(f"Segmented")
     plt.show()
-    
+
     return mask_overlay
-   
-    
+
+
 def save_predictions(filename, mask_overlay, transform, crs):
     # Save the individual segmentations into a multi-part ShapeFile
-    mask = mask_overlay.astype('int16')  # Convert the mask to integer type
+    mask = mask_overlay.astype("int16")  # Convert the mask to integer type
     results = (
-        {'properties': {'raster_val': v}, 'geometry': s}
+        {"properties": {"raster_val": v}, "geometry": s}
         for i, (s, v) in enumerate(shapes(mask, transform=transform))
         if v == 255  # Add condition to only keep 'trees'
     )
@@ -286,8 +312,8 @@ def save_predictions(filename, mask_overlay, transform, crs):
         gdf.crs = crs  # Assign the Coordinate Reference System of the original image
 
         # Save to file
-        gdf.to_file(filename, driver='GPKG')
-        
+        gdf.to_file(filename, driver="GPKG")
+
 
 def generate_tiles(image_file, size=3000):
     """Generates n x n polygon tiles.
@@ -308,13 +334,13 @@ def generate_tiles(image_file, size=3000):
 
     # Create a dictionary which will contain our 64 x 64 px polygon tiles
     # Later we'll convert this dict into a GeoPandas DataFrame.
-    geo_dict = { 'geometry' : []}
+    geo_dict = {"geometry": []}
     index = 0
 
     # Do a sliding window across the raster image
-    with tqdm(total=width*height) as pbar:
-        for w in range(0, width, int(size/2)):
-            for h in range(0, height, int(size/2)):
+    with tqdm(total=width * height) as pbar:
+        for w in range(0, width, int(size / 2)):
+            for h in range(0, height, int(size / 2)):
                 # Create a Window of your desired size
                 window = rio.windows.Window(h, w, size, size)
                 # Get the georeferenced window bounds
@@ -323,20 +349,20 @@ def generate_tiles(image_file, size=3000):
                 bbox = box(*bbox)
 
                 # Update dictionary
-                #geo_dict['id'].append(uid)
-                geo_dict['geometry'].append(bbox)
+                # geo_dict['id'].append(uid)
+                geo_dict["geometry"].append(bbox)
 
                 index += 1
-                pbar.update(size*size)
+                pbar.update(size * size)
 
     # Cast dictionary as a GeoPandas DataFrame
     results = gpd.GeoDataFrame(pd.DataFrame(geo_dict))
     # Set CRS to EPSG:4326
-    results.crs = {'init' :'epsg:4326'}
+    results.crs = {"init": "epsg:4326"}
     return results
 
 
-def show_crop(image, shape, title=''):
+def show_crop(image, shape, title=""):
     """Crops an image based on the polygon shape.
     Reference: https://rio.readthedocs.io/en/latest/api/rio.mask.html#rio.mask.mask
 
@@ -349,9 +375,11 @@ def show_crop(image, shape, title=''):
     with rio.open(image) as src:
         out_image, out_transform = rio.mask.mask(src, shape, crop=True)
         show(out_image, title=title)
-        
-        
-def predict_crop(image, text_prompt, shape, model, out_dir, uid):
+
+
+def predict_image_crop(
+    image, text_prompt, shape, model, out_dir, uid, box_threshold, text_threshold
+):
     """Generates model prediction using trained model
 
     Args:
@@ -364,9 +392,6 @@ def predict_crop(image, text_prompt, shape, model, out_dir, uid):
     """
 
     with rio.open(image) as src:
-        # Crop source image using polygon shape
-        # See more information here:
-        # https://rio.readthedocs.io/en/latest/api/rio.mask.html#rio.mask.mask
         out_image, out_transform = rio.mask.mask(src, shape, crop=True)
 
         if np.mean(out_image) == 255:
@@ -375,72 +400,81 @@ def predict_crop(image, text_prompt, shape, model, out_dir, uid):
         # Get the metadata of the source image and update it
         # with the width, height, and transform of the cropped image
         out_meta = src.meta
-        out_meta.update({
-              "driver": "GTiff",
-              "height": out_image.shape[1],
-              "width": out_image.shape[2],
-              "transform": out_transform
-        })
+        out_meta.update(
+            {
+                "driver": "GTiff",
+                "height": out_image.shape[1],
+                "width": out_image.shape[2],
+                "transform": out_transform,
+            }
+        )
 
         # Save the cropped image as a temporary TIFF file.
         if not os.path.exists(out_dir):
             os.makedirs(out_dir)
         temp_tif = os.path.join(out_dir, f"{uid}.tif")
-
         with rio.open(temp_tif, "w", **out_meta) as dest:
             dest.write(out_image)
 
-        # Open the cropped image and generated prediction
-        # using the trained Pytorch model
         out_file = os.path.join(out_dir, f"{uid}.gpkg")
-
         model_predict(
-          temp_tif,
-          model,
-          text_prompt,
-          box_threshold=0.3,
-          text_threshold=0.3,
-          out_file=out_file,
-          visualize=False
+            temp_tif,
+            model,
+            text_prompt,
+            box_threshold=box_threshold,
+            text_threshold=text_threshold,
+            out_file=out_file,
+            visualize=False,
         )
-       
-    
+
+
 def merge_polygons(gpkg_dir, crs="EPSG:4326"):
-    files = filenames = next(os.walk(gpkg_dir), (None, None, []))[2] 
+    files = filenames = next(os.walk(gpkg_dir), (None, None, []))[2]
 
     polygons = []
     for file in files:
-        if file.split('.')[-1] == 'gpkg':
+        if file.split(".")[-1] == "gpkg":
             filepath = os.path.join(gpkg_dir, file)
             polygons.append(gpd.read_file(filepath))
 
     polygons = pd.concat(polygons)
-    polygons = gpd.GeoDataFrame(polygons)[['geometry']]
+    polygons = gpd.GeoDataFrame(polygons)[["geometry"]]
     polygons = polygons.set_crs(crs, allow_override=True)
     if crs != "EPSG:4326":
         polygons = polygons.to_crs("EPSG:4326")
-        
+
+    geoms = polygons.geometry.unary_union
     polygons = gpd.GeoDataFrame(geometry=[geoms])
     polygons = polygons.explode().reset_index(drop=True)
     return polygons
 
 
 def predict_image(
-    image_file, 
-    text_prompt, 
-    tiles, 
-    model, 
-    out_dir, 
-    out_file, 
-    intersect_ratio=0.6
+    image_file,
+    text_prompt,
+    tiles,
+    model,
+    out_dir,
+    out_file,
+    box_threshold=0.3,
+    text_threshold=0.3,
 ):
     with rio.open(image_file) as src:
-        crs = src.crs  
-    
+        crs = src.crs
+
     for index in tqdm(range(len(tiles)), total=len(tiles)):
-        shape = [tiles.iloc[index]['geometry']]
-        predict_crop(image_file, text_prompt, shape, model, out_dir, index)
+        shape = [tiles.iloc[index]["geometry"]]
+        predict_image_crop(
+            image_file,
+            text_prompt,
+            shape,
+            model,
+            out_dir,
+            index,
+            box_threshold,
+            text_threshold,
+        )
 
     polygons = merge_polygons(out_dir, crs=crs)
-    polygons.to_file("output.gpkg", driver='GPKG')
+    polygons.to_file("output.gpkg", driver="GPKG")
     return polygons
