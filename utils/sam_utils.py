647ec92efa67124a2e19e52182a868b64f4c1375
Isabelle Tingzon <issatingzon@gmail.com>
2023-07-15 01:19:44 +0800

    Add MIT license

--- utils/sam_utils.py
+++ utils/sam_utils.py
@@ -1,5 +1,28 @@
+""""
+MIT License
+
+Copyright (c) 2023 Lucas Prado Osco
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 import os
-import numpy as np
 import torch
 import rasterio as rio
 import geopandas as gpd
@@ -10,7 +33,6 @@ import groundingdino.datasets.transforms as T
 from PIL import Image
 import rasterio.mask
 from rasterio.plot import show
-from matplotlib.patches import Rectangle
 from shapely.geometry import box
 
 from groundingdino.models import build_model
@@ -30,7 +52,6 @@ from rasterio.features import shapes
 from tqdm.notebook import tqdm
 import pandas as pd
 import numpy as np
-import itertools
 
 
 # Define constants
@@ -232,264 +253,3 @@ def transform_image(image: Image) -> torch.Tensor:
     )
     image_transformed, _ = transform(image, None)
     return image_transformed
-
-
-def model_predict(
-    image_file,
-    model,
-    text_prompt,
-    box_threshold=0.3,
-    text_threshold=0.3,
-    out_file=None,
-    visualize=True,
-):
-    with rio.open(image_file) as src:
-        image_np = src.read().transpose((1, 2, 0))
-        transform = src.transform
-        crs = src.crs
-    image_pil = Image.fromarray(image_np[:, :, :3])
-
-    masks, boxes, phrases, logit = model.predict(
-        image_pil,
-        text_prompt,
-        box_threshold=box_threshold,
-        text_threshold=text_threshold,
-    )
-    torch.cuda.empty_cache()
-
-    # Generate mask overlay
-    mask_overlay = np.zeros_like(image_np[..., 0], dtype=np.uint8)
-    for i, (box, mask) in enumerate(zip(boxes, masks)):
-        if isinstance(mask, torch.Tensor):
-            mask = mask.cpu().numpy().astype(np.uint8)
-        mask_overlay += ((mask > 0) * (i + 1)).astype(np.uint8)
-    mask_overlay = (mask_overlay > 0) * 255
-
-    if out_file:
-        save_predictions(out_file, mask_overlay, transform, crs)
-
-    if visualize:
-        visualize_predictions(image_pil, boxes, mask_overlay)
-
-
-def visualize_predictions(image_pil, boxes, mask_overlay):
-    plt.figure(figsize=(10, 10))
-    plt.imshow(image_pil)
-
-    for box in boxes:
-        box = box.cpu().numpy()
-        rect = patches.Rectangle(
-            (box[0], box[1]),
-            box[2] - box[0],
-            box[3] - box[1],
-            linewidth=1,
-            edgecolor="r",
-            facecolor="none",
-        )
-        plt.gca().add_patch(rect)
-
-    plt.imshow(mask_overlay, cmap="viridis", alpha=0.4)
-    plt.title(f"Segmented")
-    plt.show()
-
-    return mask_overlay
-
-
-def save_predictions(filename, mask_overlay, transform, crs):
-    # Save the individual segmentations into a multi-part ShapeFile
-    mask = mask_overlay.astype("int16")  # Convert the mask to integer type
-    results = (
-        {"properties": {"raster_val": v}, "geometry": s}
-        for i, (s, v) in enumerate(shapes(mask, transform=transform))
-        if v == 255  # Add condition to only keep 'trees'
-    )
-
-    geoms = list(results)
-    if len(geoms) > 0:
-        gdf = gpd.GeoDataFrame.from_features(geoms)
-        gdf.crs = crs  # Assign the Coordinate Reference System of the original image
-
-        # Save to file
-        gdf.to_file(filename, driver="GPKG")
-
-
-def generate_tiles(image_file, size=3000):
-    """Generates n x n polygon tiles.
-
-    Args:
-      image_file (str): Image file path (.tif)
-      output_file (str): Output file path (.geojson)
-      area_str (str): Name of the region
-      size(int): Window size
-
-    Returns:
-      GeoPandas DataFrame: Contains 64 x 64 polygon tiles
-    """
-
-    # Open the raster image using rio
-    with rio.open(image_file) as raster:
-        width, height = raster.shape
-
-    # Create a dictionary which will contain our 64 x 64 px polygon tiles
-    # Later we'll convert this dict into a GeoPandas DataFrame.
-    geo_dict = {"geometry": []}
-    index = 0
-
-    # Do a sliding window across the raster image
-    with tqdm(total=width * height) as pbar:
-        for w in range(0, width, int(size / 2)):
-            for h in range(0, height, int(size / 2)):
-                # Create a Window of your desired size
-                window = rio.windows.Window(h, w, size, size)
-                # Get the georeferenced window bounds
-                bbox = rio.windows.bounds(window, raster.transform)
-                # Create a shapely geometry from the bounding box
-                bbox = box(*bbox)
-
-                # Update dictionary
-                # geo_dict['id'].append(uid)
-                geo_dict["geometry"].append(bbox)
-
-                index += 1
-                pbar.update(size * size)
-
-    # Cast dictionary as a GeoPandas DataFrame
-    results = gpd.GeoDataFrame(pd.DataFrame(geo_dict))
-    # Set CRS to EPSG:4326
-    results.crs = {"init": "epsg:4326"}
-    return results
-
-
-def close_holes(poly: Polygon) -> Polygon:
-    """
-    Close polygon holes by limitation to the exterior ring.
-    Source: https://stackoverflow.com/a/61466689
-
-    Args:
-        poly: Input shapely Polygon
-    Example:
-        df.geometry.apply(lambda p: close_holes(p))
-    """
-    if poly.interiors:
-        return Polygon(list(poly.exterior.coords))
-    else:
-        return poly
-
-
-def show_crop(image, shape, title=""):
-    """Crops an image based on the polygon shape.
-    Reference: https://rio.readthedocs.io/en/latest/api/rio.mask.html#rio.mask.mask
-
-    Args:
-        image (str): Image file path (.tif)
-        shape (geometry): The tile with which to crop the image
-        title(str): Image title
-    """
-
-    with rio.open(image) as src:
-        out_image, out_transform = rio.mask.mask(src, shape, crop=True)
-        show(out_image, title=title)
-
-
-def predict_image_crop(
-    image, text_prompt, shape, model, out_dir, uid, box_threshold, text_threshold
-):
-    with rio.open(image) as src:
-        out_image, out_transform = rio.mask.mask(src, shape, crop=True)
-
-        if (np.mean(out_image) == 255) or (np.mean(out_image) == 0):
-            return
-
-        # Get the metadata of the source image and update it
-        # with the width, height, and transform of the cropped image
-        out_meta = src.meta
-        out_meta.update(
-            {
-                "driver": "GTiff",
-                "height": out_image.shape[1],
-                "width": out_image.shape[2],
-                "transform": out_transform,
-            }
-        )
-
-        # Save the cropped image as a temporary TIFF file.
-        if not os.path.exists(out_dir):
-            os.makedirs(out_dir)
-        temp_tif = os.path.join(out_dir, f"{uid}.tif")
-        with rio.open(temp_tif, "w", **out_meta) as dest:
-            dest.write(out_image)
-
-        out_file = os.path.join(out_dir, f"{uid}.gpkg")
-        model_predict(
-            temp_tif,
-            model,
-            text_prompt,
-            box_threshold=box_threshold,
-            text_threshold=text_threshold,
-            out_file=out_file,
-            visualize=False,
-        )
-
-
-def merge_polygons(gpkg_dir, crs, max_area, min_area, tolerance):
-    files = filenames = next(os.walk(gpkg_dir), (None, None, []))[2]
-
-    polygons = []
-    for file in files:
-        if file.split(".")[-1] == "gpkg":
-            filepath = os.path.join(gpkg_dir, file)
-            gdf = gpd.read_file(filepath)
-            gdf = gdf.set_crs(crs, allow_override=True)
-            polygons.append(gdf)
-
-    polygons = pd.concat(polygons)
-    polygons = gpd.GeoDataFrame(polygons)[["geometry"]]
-
-    geoms = polygons.geometry.unary_union
-    polygons = gpd.GeoDataFrame(geometry=[geoms], crs=crs)
-    polygons = polygons.explode().reset_index(drop=True)
-    polygons.geometry = polygons.geometry.apply(lambda p: close_holes(p))
-
-    polygons = polygons.to_crs("EPSG:32620")
-    polygons["area"] = polygons.geometry.area
-    polygons = polygons[(polygons.area < max_area) & (polygons.area > min_area)]
-
-    polygons = polygons.to_crs("EPSG:4326")
-    polygons.geometry = polygons.geometry.simplify(tolerance=tolerance)
-    polygons = polygons.to_crs(crs)
-
-    return polygons
-
-
-def predict_image(
-    image_file,
-    text_prompt,
-    tiles,
-    model,
-    out_dir,
-    out_file,
-    box_threshold=0.3,
-    text_threshold=0.3,
-    max_area=1000,
-    min_area=1,
-    tolerance=0.000005,
-):
-    with rio.open(image_file) as src:
-        crs = src.crs
-
-    for index in tqdm(range(len(tiles)), total=len(tiles)):
-        shape = [tiles.iloc[index]["geometry"]]
-        predict_image_crop(
-            image_file,
-            text_prompt,
-            shape,
-            model,
-            out_dir,
-            index,
-            box_threshold,
-            text_threshold,
-        )
-
-    polygons = merge_polygons(out_dir, crs, max_area, min_area, tolerance)
-    polygons.to_file(out_file, driver="GPKG")
-    return polygons
